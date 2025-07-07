bb3219a4699bce5b6a6a15df7f0e994590751add
sachinsoam <sachinsoam8869@gmail.com>
2025-07-07 17:05:35 +0530

    ad

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -15,7 +15,7 @@ provider "aws" {
 
 resource "aws_instance" "myserver" {
     ami = "ami-0b8607d2721c94a77"
-    instance_type = "t3.micro"
+    instance_type = "t2.micro"
   
   tags = {
     Name = "sample-server"
