38705e28b4f1cb39c053185522d24854206993f1
sachinsoam <sachinsoam8869@gmail.com>
2025-07-07 14:37:36 +0530

    make main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "5.54.1"
+        }
+    }
+}
+
+#configure the aws
+provider "aws" {
+    region = "apsoutheast-1"
+  
+}
+
+resource "aws_instance" "myserver" {
+    ami = "ami-0b8607d2721c94a77"
+    instance_type = "t2.micro"
+  
+}
